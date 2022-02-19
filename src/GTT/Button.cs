a580d906c45bdd38df6df9e6134e47161d93c813
Ashish <sam4lordjesus@outlook.com>
2022-02-20 00:04:53 +0530

    button

--- src/GTT/Button.cs
+++ src/GTT/Button.cs
@@ -1,46 +1,52 @@
 using Azuxiren.MG;
 using Azuxiren.MG.Menu;
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 namespace GTT
 {
-	public class Button : AbstractButton
+	public class Button : AbstractButton, IMenuItem
 	{
-		private readonly static Texture2D patch = GameApp.CurrentGame.patch;
-		private readonly static SpriteBatch batch = GameApp.CurrentGame._spriteBatch;
-		private readonly static IInputManager input = GameApp.CurrentGame.input;
-		private readonly static SpriteFont font = GameApp.CurrentGame.font;
-		private TextBox box;
-		private Color backcolor;
-		public override bool InputPressed 
-		{ 
-			get => bounds.Contains(input.PointerLocation) && input.Clicked; 
-			set { } 
-		}
-		public override bool Selected 
-		{ 
-			get => bounds.Contains(input.PointerLocation); 
-			set { } 
-		}
-		public Button(Rectangle bounds, string Message = "", bool EnableAtStart = true) : base(bounds, Message, EnableAtStart)
+		private readonly static Texture2D Patch = GameApp.CurrentGame.Patch;
+		private readonly static SpriteBatch Batch = GameApp.CurrentGame.SpriteBatch;
+		private readonly static IInputManager Input = GameApp.CurrentGame.Input;
+		private readonly static SpriteFont Font = GameApp.CurrentGame.Font;
+		private TextBox _box;
+		private Color _backcolor;
+		public override bool InputPressed
 		{
-			box = new TextBox(bounds, Message, font, Color.White);
+			get => Bounds.Contains(Input.PointerLocation) && Input.Clicked;
+			set { }
+		}
+		public override bool Selected
+		{
+			get => Bounds.Contains(Input.PointerLocation);
+			set { }
+		}
+		public Button(Rectangle bounds, string message = "", bool enableAtStart = true) : base(bounds, message, enableAtStart)
+		{
+			_box = new TextBox(bounds, message, Font, Color.White);
 		}
 		public override void LoadContent() { }
 		public override void Draw(GameTime gt)
 		{
 			switch (State)
 			{
-				case ComponentState.UnSelected: backcolor = Color.Black; break;
-				case ComponentState.Press: backcolor = Color.White; break;
-				case ComponentState.Selected: backcolor=Color.Silver; break;
-				case ComponentState.Release: backcolor = Color.Silver; break;
+				case ComponentState.UnSelected: _backcolor = Color.Black; break;
+				case ComponentState.Press: _backcolor = Color.White; break;
+				case ComponentState.Selected: _backcolor = Color.Silver; break;
+				case ComponentState.Release: _backcolor = Color.Silver; break;
 				case ComponentState.Disabled:
-					backcolor = Color.DarkRed;
+					_backcolor = Color.DarkRed;
 					break;
 			}
-			batch.Draw(patch, bounds, backcolor);
-			box.Draw(batch);
+			Batch.Draw(Patch, Bounds, _backcolor);
+			_box.Draw(Batch);
+		}
+		public void Set(Rectangle bds)
+		{
+			Bounds = bds;
+			_box.Bounds = bds;
 		}
 	}
 }
