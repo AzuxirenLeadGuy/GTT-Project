e2bbfc958d8c228780d308314bb1115f272029a1
Ashish <sam4lordjesus@outlook.com>
2022-02-21 23:32:25 +0530

    line

--- src/GTT/LineObject.cs
+++ src/GTT/LineObject.cs
@@ -1,41 +1,42 @@
-using Microsoft.Xna.Framework.Graphics;
 using System;
+
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 namespace GTT
 {
 	public class LineObject
 	{
 		protected readonly static Vector2 Origin = new(0f, 0.5f);
-		protected readonly static Texture2D tex = GameApp.CurrentGame.patch;
-		protected readonly static Texture2D head = GameApp.CurrentGame.triangle;
-		protected readonly static Vector2 arrowcenter = new(head.Width / 2, head.Height - 1);
-		public Vector2 point1, Scale;
-		public Rectangle arrow;
-		public float Angle, arrowAngle;
-		public LineObject(Point start, Point end, float thickness)
+		protected readonly static Texture2D Tex = GameApp.CurrentGame.Patch;
+		protected readonly static Texture2D Head = GameApp.CurrentGame.Triangle;
+		protected readonly static Vector2 Arrowcenter = new(Head.Width / 2, Head.Height - 1);
+		private Vector2 _point1, _scale;
+		private Rectangle _arrow;
+		private float _angle, _arrowAngle;
+		public LineObject(Point start, Point end, float thickness = 1f)
 		{
-			point1 = Scale = Vector2.Zero;
-			Angle = 0;
-			arrow = Rectangle.Empty;
+			_point1 = _scale = Vector2.Zero;
+			_angle = 0;
+			_arrow = Rectangle.Empty;
 			Compute(start, end, thickness);
 		}
 		public void Compute(Point start, Point end, float thickness = 1f)
 		{
-			point1 = start.ToVector2();
+			_point1 = start.ToVector2();
 			var point2 = end.ToVector2();
 			byte len = 5;
-			arrow.Width = arrow.Height = 2 * len;
-			arrow.X = end.X;
-			arrow.Y = end.Y;
-			Scale = new Vector2(Vector2.Distance(point1, point2), thickness);
-			Angle = (float)Math.Atan2(point2.Y - point1.Y, point2.X - point1.X);
+			_arrow.Width = _arrow.Height = 2 * len;
+			_arrow.X = end.X;
+			_arrow.Y = end.Y;
+			_scale = new Vector2(Vector2.Distance(_point1, point2), thickness);
+			_angle = (float)Math.Atan2(point2.Y - _point1.Y, point2.X - _point1.X);
 			float radian90 = (float)(Math.PI / 2);
-			arrowAngle = Angle + radian90;
+			_arrowAngle = _angle + radian90;
 		}
 		public void Draw(SpriteBatch batch, Color color)
 		{
-			batch.Draw(tex, point1, null, color, Angle, Origin, Scale, SpriteEffects.None, 0);
-			batch.Draw(head, arrow, null, color, arrowAngle, arrowcenter, SpriteEffects.None, 0);
+			batch.Draw(Tex, _point1, null, color, _angle, Origin, _scale, SpriteEffects.None, 0);
+			batch.Draw(Head, _arrow, null, color, _arrowAngle, Arrowcenter, SpriteEffects.None, 0);
 		}
 	}
 
