1bcda425c83ac22d26bf9a03af189ca880906959
Ashish <sam4lordjesus@outlook.com>
2022-02-24 22:45:52 +0530

    welcome

--- src/GTT/WelcomeScreen.cs
+++ src/GTT/WelcomeScreen.cs
@@ -1,50 +1,50 @@
-using Azuxiren.MG;
-using Microsoft.Xna.Framework;
 using System.Collections.Generic;
-using Microsoft.Xna.Framework.Graphics;
+
+using Azuxiren.MG;
 using Azuxiren.MG.Menu;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GTT
 {
 	public struct WelcomeScreen : IScreen
 	{
 		enum State : byte { Welcome, SelectAlgo, EnterGraph };
-		State state;
+		private State _state;
 		private Button _exitButton;
-		internal SpriteBatch batch;
-		internal Texture2D patch;
-		internal MovableObject o1, o2, o3;
+		internal readonly SpriteBatch Batch = GameApp.CurrentGame.SpriteBatch;
+		internal readonly Texture2D Patch = GameApp.CurrentGame.Patch;
+		internal MovableObject O1, O2, O3;
 		public void LoadContent()
 		{
-			batch = GameApp.CurrentGame._spriteBatch;
-			patch = GameApp.CurrentGame.patch;
 			var bound = GameApp.CurrentGame.Window.ClientBounds;
-			state = State.Welcome;
+			_state = State.Welcome;
 			bound = new Rectangle(0, 0, bound.Width, bound.Height);
 			var x = new Rectangle(0, 0, 140, 60);
 			Global.SetCenter(ref x, bound);
 			_exitButton = new Button(x, "Exit");
 			_exitButton.OnRelease += (o, e) => GameApp.CurrentGame.Exit();
-			o1.LoadContent();
-			o2.LoadContent();
-			o2.Place(new Point(0, 100));
-			o2._color = Color.Blue;
-			o3.LoadContent();
-			o3.Place(new Point(100, 0));
-			o3._color = Color.Green;
+			MovableObjectManager mgr = new()
+			{
+				Region = new Rectangle(0, 0, 400, 400)
+			};
+			O1 = new MovableObject(mgr, new Rectangle(0, 0, 20, 20), Color.Red);
+			O2 = new MovableObject(mgr, new Rectangle(0, 0, 20, 20), Color.Blue);
+			O3 = new MovableObject(mgr, new Rectangle(0, 0, 20, 20), Color.Green);
 		}
 		public void Update(GameTime gt)
 		{
-			o1.Update();
-			o2.Update();
-			o3.Update();
+			O1.Update();
+			O2.Update();
+			O3.Update();
 			_exitButton.Update(gt);
 		}
 		public void Draw(GameTime gt)
 		{
-			o3.Draw();
-			o2.Draw();
-			o1.Draw();
+			O3.Draw();
+			O2.Draw();
+			O1.Draw();
 			_exitButton.Draw(gt);
 		}
 	}
